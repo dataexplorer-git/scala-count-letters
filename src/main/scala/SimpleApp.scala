82cd47599fff5b75367b8b40ab496e2d00ce3d9f
hari.k.chintala <Hari.Chintala@kindredplc.com>
2017-03-24 15:49:59 +0000

    added version 1.1 with differnt println

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -11,6 +11,6 @@ object SimpleApp {
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
-    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs)) 
   }
 }
